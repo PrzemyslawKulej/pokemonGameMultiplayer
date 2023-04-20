ff8a28bf063dd903b949d092dab5d62eb3547a15
PrzemyslawKulej <przemek.kulej1@gmail.com>
2023-04-20 23:44:00 +0200

    added some comented code, working on cards color change due to the type

--- app.js
+++ app.js
@@ -1,7 +1,29 @@
 const pokedex = document.getElementById('pokedex');
 
+
+
 let limit = 20;
 let i = 0;
+
+// Object of types and colors to style the cards depends on pokemons type 
+
+const colors = {
+	fire: '#FDDFDF',
+	grass: '#DEFDE0',
+	electric: '#FCF7DE',
+	water: '#DEF3FD',
+	ground: '#f4e7da',
+	rock: '#d5d5d4',
+	fairy: '#fceaff',
+	poison: '#98d7a5',
+	bug: '#f8d5a3',
+	dragon: '#97b3e6',
+	psychic: '#eaeda1',
+	flying: '#F5F5F5',
+	fighting: '#E6E0D4',
+	normal: '#F5F5F5'
+};
+const main_types = Object.keys(colors);
 
 // Fetching data from API and cooldown button during the fetching
 
@@ -26,10 +48,17 @@ const fetchPokemon = () => {
         const pokemon = results.map((result) => ({
             name: result.name,
             image: result.sprites['front_default'],
-            type: result.types.map((type) => type.type.name).join(', '),
+            type: result.types.map((type) => type.type.name),
             id: result.id
         }));
         displayPokemon(pokemon);
+        // const poke_types = pokemon.types.map(type => type.type.name);
+        // const type = main_types.find(type => poke_types.indexOf(type) > -1);
+	    // const pokemonEl = document.getElementsByClassName('card');
+        // const color = colors[type];
+        // (() => {
+        //     pokemonEl.style.backgroundColor = color;
+        // })
     });
 };
 
@@ -46,9 +75,17 @@ const displayPokemon = (pokemon) => {
             <p class="card-subtitle">Type: ${pokeman.type}</p>
         </li>
     `
+    
         )
         .join('');
     pokedex.innerHTML = pokemonHTMLString;
+
+
+	
+    (() => {
+        pokemonEl.style.backgroundColor = color;
+    })
+	
 };
 
 // Creating button 
